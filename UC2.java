fe9a5dcb8af1319e6b6dc117dfbaf5050e002b16
sk6885-netizen <sk6885@srmist.edu.in>
2026-04-06 15:12:20 +0530

    UC2

---
+++ UC2.java
@@ -0,0 +1,29 @@
+/**
+ * OOPSBannerApp UC2 - OOPS Banner Display Application
+ *
+ * This class demonstrates displaying the OOPS acronym as a large banner
+ * using asterisks (*) and spaces with individual print statements.
+ *
+ * @author Developer
+ * @version 2.0
+ */
+public class UC2 {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(" *******  " + " *******  " + " *******  " + "  ****** ");
+        // Line 2
+        System.out.println("*       * " + "*       * " + "*       * " + " *      *");
+        // Line 3
+        System.out.println("*       * " + "*       * " + "*       * " + " *       ");
+        // Line 4
+        System.out.println("*       * " + "*       * " + "********* " + "  ****** ");
+        // Line 5
+        System.out.println("*       * " + "*       * " + "*         " + "        *");
+        // Line 6
+        System.out.println("*       * " + "*       * " + "*         " + " *      *");
+        // Line 7
+        System.out.println(" *******  " + " *******  " + "*         " + "  ****** ");
+    }
+}
